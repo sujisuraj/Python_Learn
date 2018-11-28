a5a91000a56387d6a19fc2f657e0f9ceea739177
sujisuraj <Chachu@2015>
2018-11-28 13:29:20 -0500

    Merge_param - utility to merge multiple text files from subdirectories into one file, for EDC informatica pwc parameter file

--- L1.py
+++ L1.py
@@ -1,12 +1,3 @@
-import pandas as pd
-import datetime
-from pandas_datareader import data as web
-import matplotlib.pyplot as plt
-from matplotlib import style
-style.use('ggplot')
-start=datetime.datetime(2012,1,1)
-end=datetime.datetime(2015,1,1)
-df=web.DataReader("XOM","yahoo",start,end)
-print(df.head())
-df['Adj Close'].plot()
-plt.show()
+import tensorflow as tf
+from tensorflow import keras
+
